ae77a865802b73ea1130fb521d5981f7b0021bfc
gagagon <124789502+gagagon@users.noreply.github.com>
2023-03-15 13:46:32 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,43 @@
+input.onButtonPressed(Button.A, function () {
+    if (led.point(2, 2)) {
+        led.plot(1, 2)
+        led.unplot(2, 2)
+    } else if (led.point(1, 2)) {
+        led.plot(0, 2)
+        led.unplot(1, 2)
+    } else if (led.point(3, 2)) {
+        led.plot(2, 2)
+        led.unplot(3, 2)
+    } else if (led.point(4, 2)) {
+        led.plot(3, 2)
+        led.unplot(4, 2)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (led.point(2, 2)) {
+        led.plot(3, 2)
+        led.unplot(2, 2)
+    } else if (led.point(1, 2)) {
+        led.plot(2, 2)
+        led.unplot(1, 2)
+    } else if (led.point(3, 2)) {
+        led.plot(4, 2)
+        led.unplot(3, 2)
+    } else if (led.point(0, 2)) {
+        led.plot(1, 2)
+        led.unplot(0, 2)
+    }
+})
+for (let index = 0; index <= 4; index++) {
+    led.plot(0, 2)
+    led.plot(1, 2)
+    led.plot(2, 2)
+    led.plot(3, 2)
+    led.plot(4, 2)
+}
+basic.pause(2000)
+basic.clearScreen()
+led.plot(2, 2)
+basic.forever(function () {
+	
+})

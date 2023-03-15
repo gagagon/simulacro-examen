6d450ec5e05d4b2fea0999b5a6c76db577883800
gagagon <124789502+gagagon@users.noreply.github.com>
2023-03-15 13:57:48 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,3 @@
-input.onButtonPressed(Button.A, function () {
-    if (led.point(2, 2)) {
-        led.plot(1, 2)
-        led.unplot(2, 2)
-    } else if (led.point(1, 2)) {
-        led.plot(0, 2)
-        led.unplot(1, 2)
-    } else if (led.point(3, 2)) {
-        led.plot(2, 2)
-        led.unplot(3, 2)
-    } else if (led.point(4, 2)) {
-        led.plot(3, 2)
-        led.unplot(4, 2)
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    if (led.point(2, 2)) {
-        led.plot(3, 2)
-        led.unplot(2, 2)
-    } else if (led.point(1, 2)) {
-        led.plot(2, 2)
-        led.unplot(1, 2)
-    } else if (led.point(3, 2)) {
-        led.plot(4, 2)
-        led.unplot(3, 2)
-    } else if (led.point(0, 2)) {
-        led.plot(1, 2)
-        led.unplot(0, 2)
-    }
-})
 for (let index = 0; index <= 4; index++) {
     led.plot(0, 2)
     led.plot(1, 2)
@@ -37,4 +7,3 @@ for (let index = 0; index <= 4; index++) {
 }
 basic.pause(2000)
 basic.clearScreen()
-led.plot(2, 2)
